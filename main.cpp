d39e38896b1271c5c2127c3fad37def26482115b
Vabasou <beiga.vytautas@gmail.com>
2021-09-16 18:25:40 +0300

    File reading with buffer

--- main.cpp
+++ main.cpp
@@ -1,8 +1,49 @@
 #include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
+stringstream getText(string &text) {
+    std::ifstream file;
+    file.open(text);
+
+    if(!file) {
+        throw std::runtime_error("File could not be opened");
+    }
+
+    stringstream buffer;
+    buffer << file.rdbuf();
+    file.close();
+
+    return buffer;
+}
+
+string getTextAsString(string &text) {
+    stringstream buffer = getText(text);
+    string content = buffer.str();
+    return content;
+}
+
+void saveText(stringstream buffer, string text) {
+    std::ofstream file;
+    file.open(text);
+
+    if(!file) {
+        throw std::runtime_error("File could not be opened");
+    }
+
+    file << buffer.str();
+    file.close();
+}
+
+// string file(string &file) {
+//     string content = getTextAsString(file);
+// }
+
 int main()
 {
-    printf("Hello world!");
+    string text = "test.txt";
+    cout << getTextAsString(text);
 }   
